3fe67521070e2737c11c85adcdff8e9b2366ab3f
Sheraz <banckluser1@yahoo.com>
2012-07-02 11:51:08 +0500

    2nd commit

---
+++ SigninToken.cs
@@ -0,0 +1,25 @@
+﻿/*--------------------------------------------------------------------------------------+
+|   
+|  This sample is developed and maintained by CodePorting venture
+|  [http://codeporting.com ] a division of Aspose Pty Ltd.
+|
++--------------------------------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodePortingCSharp2Java
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    static public class SigninToken
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string Token = string.Empty;   
+
+    }
+}
