43bb2544ae10be219ea9db629d62f52b95812c5c
Sheraz <banckluser1@yahoo.com>
2012-07-02 12:42:38 +0500

    new file

---
+++ CodePortTest.cs
@@ -0,0 +1,18 @@
+﻿/*--------------------------------------------------------------------------------------+
+|   
+|  This sample is developed and maintained by CodePorting venture
+|  [http://codeporting.com ] a division of Aspose Pty Ltd.
+|
++--------------------------------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NameSpaceData
+{
+    static public class CodePortTest
+    {
+        public integer count = 0;
+    }
+}
